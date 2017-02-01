e5a51201ee6f27e86d607e6e0680ce65d489a520
Corey Slaven <coreyslaven@aol.com>
2017-02-01 10:24:47 -0500

    add environment variables

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key: '7lRlrzaGgwHo3ALmxqrjfFCWp',
-  consumer_secret: '1UHwNFh0GpwnztPEgwe38WKcTPMihe3DWaMyYafm2FlKp7C2TR',
-  access_token: '808808674508541956-zSbHF2rj68vMp7t7lXpAvZpwyZfN5AF',
-  access_token_secret: 'iCqn5ZJUXbxROzM9bVK1xcwzNVoiZDWspAEyoJTZUWNNi'
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 }
