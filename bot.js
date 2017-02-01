9743e6526da783af5a3e7321229ce5e3ce2d92a8
Corey Slaven <coreyslaven@aol.com>
2017-02-01 10:18:40 -0500

    initial commit

---
+++ bot.js
@@ -0,0 +1,79 @@
+var twit = require('twit');
+var config = require('./config.js');
+
+var Twitter = new twit(config);
+
+var ranDom = function (arr) {
+  var index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+}
+
+//retweet a random tweet
+var retweet = function () {
+  var params = {
+    q: '@floridacomeup OR #FloridaHipHop OR #5MMA OR #floridarap OR #mieuxfl OR @MIEUX_FL OR @CitrusRap',
+    lang: 'en'
+  }
+
+  Twitter.get('search/tweets', params, function (err,data) {
+
+    var tweet = data.statuses;
+    var randomTweet = ranDom(tweet);
+
+    if (typeof randomTweet !== 'undefined') {
+      Twitter.post('statuses/retweet/:id', {
+        id: randomTweet.id_str
+      }, function (err,response) {
+        if (response) {
+          console.log('Retweeted!');
+        }
+
+        if (err) {
+          console.log('Cannot be retweeted :( something went wrong');
+        }
+      });
+    }
+    else {
+      console.log('Something went wrong while searching for tweet');
+    }
+  })
+}
+
+retweet();
+setInterval(retweet, 60000 * 10);
+
+//favorite a random tweet
+var favoriteTweet = function () {
+
+  var params = {
+    q: '@floridacomeup OR #FloridaHipHop OR #5MMA OR #floridarap OR #mieuxfl OR @MIEUX_FL OR @CitrusRap',
+    lang: 'en'
+  }
+
+  Twitter.get('search/tweets', params, function (err, data) {
+
+    var tweet = data.statuses;
+    var randomTweet = ranDom(tweet);
+
+    if (typeof randomTweet !== 'undefined') {
+      Twitter.post('favorites/create', {
+        id: randomTweet.id_str
+      }, function (err,response) {
+        if (response) {
+          console.log('Favorited!');
+        }
+
+        if (err) {
+          console.log('Cannot be favorited :( something went wrong');
+        }
+      });
+    }
+    else {
+      console.log('Something went wrong while searching for tweet');
+    }
+  })
+}
+
+
+favoriteTweet();
+setInterval(favoriteTweet, 60000 * 15);
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: '7lRlrzaGgwHo3ALmxqrjfFCWp',
+  consumer_secret: '1UHwNFh0GpwnztPEgwe38WKcTPMihe3DWaMyYafm2FlKp7C2TR',
+  access_token: '808808674508541956-zSbHF2rj68vMp7t7lXpAvZpwyZfN5AF',
+  access_token_secret: 'iCqn5ZJUXbxROzM9bVK1xcwzNVoiZDWspAEyoJTZUWNNi'
+}
